d2cb848cc6ccc25ea99858e83e28cfd70eaeecf6
Mamedov Ramil <926556@mail.ru>
2024-07-06 23:34:57 +0300

    add post request and cors

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from flask import Flask, request, jsonify
-import jamspell
-import locale
-import os
-
-os.environ['LANG'] = 'en_US.UTF-8'
-os.environ['LANGUAGE'] = 'en_US:en'
-os.environ['LC_ALL'] = 'en_US.UTF-8'
-
-locale.setlocale(locale.LC_ALL, 'en-US')
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/get_answer', methods=['GET'])
 def get_answer():
@@ -18,14 +12,10 @@ def get_answer():
 @app.route('/process_text', methods=['POST'])
 def process_text():
     data = request.get_json()
-
     if not data or 'text' not in data:
         return jsonify({'error': 'Invalid input'}), 400
-    
-    corrector = jamspell.TSpellCorrector()
-    corrector.LoadLangModel('wiki+text/model_twiki.bin')
-    
-    text = corrector.FixFragment('Цвæр царæгай кæнæ зæйæгой нæ зæронт кæны')
+
+    text = data['text']
     changed_text = text
 
     return jsonify({'changed_text': changed_text})
