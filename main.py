d39e85f9415118a98c7ebb2fd3e08cc4d0be37b6
Mamedov Ramil <926556@mail.ru>
2024-07-06 23:34:57 +0300

    add post request

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def get_answer():
     return jsonify({"answer": 'amogus'})
 
+@app.route('/process_text', methods=['POST'])
+def process_text():
+    data = request.get_json()
+    if not data or 'text' not in data:
+        return jsonify({'error': 'Invalid input'}), 400
+    
+    text = data['text']
+    changed_text = text
+
+    return jsonify({'changed_text': changed_text})
+
 if __name__ == '__main__':
     app.run(debug=True)
