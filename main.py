3e6636920c0735ac9d62410c2345073e3f22926c
Mamedov Ramil <926556@mail.ru>
2024-07-18 12:31:20 +0300

    fix model and refactor

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import jamspell
 
 app = Flask(__name__)
 
 CORS(app)
+
+corrector = jamspell.TSpellCorrector()
+corrector.LoadLangModel('wiki+text/model_twiki.bin')
 
 @app.route('/get_answer', methods=['GET'])
 def get_answer():
@@ -12,10 +16,12 @@ def get_answer():
 @app.route('/process_text', methods=['POST'])
 def process_text():
     data = request.get_json()
+
     if not data or 'text' not in data:
         return jsonify({'error': 'Invalid input'}), 400
-
-    text = data['text']
+    
+    # text = corrector.FixFragment('Цвæр царæгай кæнæ зæйæгой нæ зæронт кæны')
+    text = corrector.FixFragment(data)
     changed_text = text
 
     return jsonify({'changed_text': changed_text})
